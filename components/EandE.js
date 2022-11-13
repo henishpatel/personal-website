813f440c7b301ead2c47f3bd2c0f58f8e7ba4f7a
Henish Patel <53644709+henishpatel@users.noreply.github.com>
2022-11-13 16:14:17 -0500

    Update
    minor change

--- components/EandE.js
+++ components/EandE.js
@@ -1,60 +1,57 @@
-import styles from '../styles/EandE.module.scss'
-import { Text, Timeline } from '@mantine/core'
-import { Book2, Briefcase, Building } from 'tabler-icons-react'
+import styles from "../styles/EandE.module.scss";
+import { Text, Timeline } from "@mantine/core";
+import { Book2, Briefcase, Building } from "tabler-icons-react";
 function EandE() {
-  return (
-    <div className={styles.eandeContainer}>
-      <h1 className="title">Experience and Education</h1>
-      <div className={styles.timelineContainer}>
-        <Timeline lineWidth={3} radius="md">
-          <Timeline.Item
-            title="Master of Applied Computing"
-            bulletSize={50}
-            bullet={<Book2 size={25} />}
-            className={styles.timelineItem}
-          >
-            <Text color="dimmed" size="md">
-              University of Windsor
-            </Text>
-            <Text size="md" mt={4}>
-              May 2021 - Present
-            </Text>
-          </Timeline.Item>
+	return (
+		<div className={styles.eandeContainer}>
+			<h1 className="title">Experience and Education</h1>
+			<div className={styles.timelineContainer}>
+				<Timeline lineWidth={3} radius="md">
+					<Timeline.Item
+						title="Master of Applied Computing"
+						bulletSize={50}
+						bullet={<Book2 size={25} />}
+						className={styles.timelineItem}
+					>
+						<Text color="dimmed" size="md">
+							University of Windsor
+						</Text>
+						<Text size="md" mt={4}>
+							May 2021 - Present
+						</Text>
+					</Timeline.Item>
 
-          <Timeline.Item
-            title="Web Development Traniee"
-            bulletSize={50}
-            bullet={<Building size={25} />}
-            className={styles.timelineItem}
-          >
-            <Text color="dimmed" size="md">
-              Pepcoding
-            </Text>
-            <Text size="md" mt={4}>
-              Sept 2021 – Feb 2021
-            </Text>
-          </Timeline.Item>
+					<Timeline.Item
+						title="Web Development Trainee"
+						bulletSize={50}
+						bullet={<Building size={25} />}
+						className={styles.timelineItem}
+					>
+						<Text color="dimmed" size="md">
+							Pepcoding
+						</Text>
+						<Text size="md" mt={4}>
+							Sept 2021 – Feb 2021
+						</Text>
+					</Timeline.Item>
 
-          
-
-          
-          <Timeline.Item
-            title="Bachelor of Technology in Computer Science and Engineering"
-            bulletSize={50}
-            bullet={<Book2 size={25} />}
-            className={styles.timelineItem}
-          >
-            <Text color="dimmed" size="md">
-              Gujarat Technological University
-            </Text>
-            <Text size="md" mt={4}>
-              Aug 2017 – Aug 2021
-            </Text>
-          </Timeline.Item>
-        </Timeline>
-      </div>
-    </div>
-  )
+					<Timeline.Item
+						title="Bachelor of Technology in Computer Science and Engineering"
+						bulletSize={50}
+						bullet={<Book2 size={25} />}
+						className={styles.timelineItem}
+					>
+						<Text color="dimmed" size="md">
+							Gujarat Technological University
+						</Text>
+						<Text size="md" mt={4}>
+							Aug 2017 – Aug 2021
+						</Text>
+					</Timeline.Item>
+				</Timeline>
+			</div>
+		</div>
+	);
 }
 
-export default EandE
+export default EandE;
